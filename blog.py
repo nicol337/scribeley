d3bee2f2de69f90c405921318d74ac4330b48d24
nicol337 <nkl9092@gmail.com>
2013-12-15 02:35:14 -0500

    -f working on get and post requests and added blog.py

--- blog.py
+++ blog.py
@@ -14,69 +14,71 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+DEFAULT_BLOG_TITLE = 'default_blog'
+DEFAULT_BLOG_POST_TITLE = 'New_Post'
 
-MAIN_PAGE_FOOTER_TEMPLATE = """\
-    <form action="/sign?%s" method="post">
-      <div><textarea name="content" rows="3" cols="200"></textarea></div>
-      <div><input type="submit" value="Sign Blog"></div>
-    </form>
+# blog = Blog(parent=user.key(),author=user.key(),title=DEFAULT_BLOG_POST_TITLE)
+class Blog(db.Model):
+    author = db.UserProperty(required=True)
+    title = db.StringProperty(required=True)
 
-    <hr>
+def Blog_Key(title):
+    return db.Key('Blog', title)
 
-    <form>Blog name:
-      <input value="%s" name="blog_name">
-      <input type="submit" value="switch">
-    </form>
+def User_Key(user):
+    return db.Key('User', user)
 
-    <a href="%s">%s</a>
+def get_posts(blog_title, limit_count):
+    results = Blogpost.all()
+    results.ancestor(blog_key(blog_title))
+    results.order("-date")
+    if limit_count:
+        results.fetch(limit_count)
+    return results
 
-  </body>
-</html>
-"""
+def get_posts_with_tag(blog_title, tag, limit_count):
+    results = Blogpost.all()
+    results.ancestor(blog_key(blog_title))
+    if tag:
+        results.filter("tags =", tag)
+    results.order("-date")
+    if limit_count:
+        results.fetch(limit_count)
+    return results
 
-DEFAULT_BLOG_NAME = 'default_blog'
+def get_blogs(user_name, limit_count):
+    results = Blog.all()
+    results.ancestor(user_key(user_name))
+    if limit_count:
+        results.fetch(limit_count)
+    return results
 
-# We set a parent key on the 'Blogposts' to ensure that they are all in the same
-# entity group. Queries across the single entity group will be consistent.
-# However, the write rate should be limited to ~1/second.
-
-def blog_key(blog_name=DEFAULT_BLOG_NAME):
-    """Constructs a Datastore key for a Blog entity with blog_name."""
-    return ndb.Key('Blog', blog_name)
-
-
-
-
-# b = Blog(author=users.get_current_user(), title="Blog Title")
-# b.put()
-# user = [users.get_current_user()]
-# blogs_of_user = db.GqlQuery("SELECT * FROM Blog WHERE author IN :1", user)
-# for blog in blogs_of_user:
-#   blog.display stuff here
 
 class Blogpost(db.Model):
     author = db.UserProperty(required=True)
-    blog = 
-    content = db.StringProperty(indexed=False)
+    title = db.StringProperty(required=True)
+    content = db.TextProperty()
+    tags = db.StringListProperty()
+    # results = db.GqlQuery("SELECT * FROM Blogpost WHERE tags = 'tech'")
+    # get the current blog's name
+    # blogpost = Blogpost(parent=blog_key(blog_name), author = self.request.get('current_user'), title=self.request.get('blog_title'))
     date = db.DateTimeProperty(auto_now_add=True)
 
-
-
-class MainPage(webapp2.RequestHandler):
+class HomePage(webapp2.RequestHandler):
 
     def get(self):
         blog_name = self.request.get('blog_name',
-                                          DEFAULT_BLOG_NAME)
-        blogposts_query = Blogpost.query(
-            ancestor=blog_key(blog_name)).order(-Blogpost.date)
-        blogposts = blogposts_query.fetch(10)
+        #                                   DEFAULT_BLOG_TITLE)
+        # blogposts_query = Blogpost.query(
+        #     ancestor=blog_key(blog_name)).order(-Blogpost.date)
+        # blogposts = blogposts_query.fetch(10)
 
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
+        # if users.get_current_user():
+        #     url = users.create_logout_url(self.request.uri)
+        #     url_linktext = 'Logout'
+        # else:
+        #     url = users.create_login_url(self.request.uri)
+        #     url_linktext = 'Login'
 
         template_values = {
             'blogposts': blogposts,
@@ -88,40 +90,7 @@ class MainPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('blog_home.html')
         self.response.write(template.render(template_values))
 
-class Blog_object(db.Model):
-    author = ndb.UserProperty(required=True)
-    title = ndb.StringProperty(required=True, indexed=False)
-
-    def get_ten_posts(self):
-        # query for blogposts which belong to this author and this blog
-
-    def get_all_posts(self):
-
-
-class Blog(webapp2.RequestHandler):
-
-    def post(self):
-        # We set the same parent key on the 'Blogpost' to ensure each Blogpost
-        # is in the same entity group. Queries across the single entity group
-        # will be consistent. However, the write rate to a single entity group
-        # should be limited to ~1/second.
-        blog_name = self.request.get('blog_name',
-                                          DEFAULT_BLOG_NAME)
-        blogpost = Blogpost(parent=blog_key(blog_name))
-
-        if users.get_current_user():
-            blogpost.author = users.get_current_user()
-
-        blogpost.content = self.request.get('content')
-        blogpost.put()
-
-        query_params = {'blog_name': blog_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
-
-
-
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', HomePage),
     ('/sign', Blog),
 ], debug=True)
-
