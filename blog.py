74a0ff1490293ff49bddcf05a279471543d2fb3b
nicol337 <nkl9092@gmail.com>
2015-10-13 15:06:19 -0400

    building getBlogs and getBlogPosts functions, trying to remedy owner of blog bug

--- blog.py
+++ blog.py
@@ -3,8 +3,6 @@ import cgi
 import urllib
 import re
 
-
-
 from google.appengine.api import users
 from google.appengine.ext import ndb
 from google.appengine.ext import db
@@ -22,12 +20,26 @@ ViewingPage = 0
 def getBlogs(authorID, blog_name = None):
     if blog_name:
         return db.GqlQuery("SELECT * FROM Blog " +
-            "WHERE title = :1 AND authorID = :2" , blog_name, authorID)
+            "WHERE authorID = :1 AND title = :2 " +
+            "ORDER BY title" , blog_name, authorID)
     else:
         return db.GqlQuery("SELECT * FROM Blog " +
-            "WHERE authorID = :1" , authorID)
+            "WHERE authorID = :1 " + 
+            "ORDER BY title", authorID)
 
-
+def getBlogPosts(authorID, blog_name, blog_post_name = None, tag_name = None):
+    if blog_post_name:
+        return db.GqlQuery("SELECT * FROM Blogpost " +
+            "WHERE authorID = :1 AND blog = :2 AND title = :3 " +
+            "ORDER BY date DESC", authorID, blog_name, blog_post_name)
+    elif tag_name:
+        return db.GqlQuery("SELECT * FROM Blogpost " +
+            "WHERE authorID = :1 AND blog = :2 AND tags = :3 " +
+            "ORDER BY date DESC", authorID, blog_name, tag_name)
+    else:
+        return db.GqlQuery("SELECT * FROM Blogpost " +
+            "WHERE authorID = :1 AND blog = :2 " +
+            "ORDER BY date DESC", authorID, blog_name)
 
 def to_link(str):
     new_link='<a href="'+str+'">'+str+'</a>'
@@ -48,7 +60,7 @@ class Blogpost(db.Model):
     blog = db.StringProperty(required=True)
     date = db.DateTimeProperty(auto_now_add=True)
 
-    def update(self,new_title,new_content, new_tags):
+    def update(self, new_title, new_content, new_tags):
         self.title = new_title
         self.content = new_content
         self.tags = new_tags
@@ -98,13 +110,11 @@ class HomePage(webapp2.RequestHandler):
 
 class UserHome(webapp2.RequestHandler):
 
-
     def post(self):
         """Create a new blog for the logged-in user."""
-        blogTitle = self.request.get('blog_title')
+        new_blog_name = self.request.get('blog_title')
         user = users.get_current_user()
-        if blogTitle:
-            new_blog_name = blogTitle
+        if new_blog_name:
             new_authorID = user.user_id()
             new_authorNickname = user.nickname()
             new_blog = Blog(authorID=new_authorID,authorNickname=new_authorNickname,title=new_blog_name)
@@ -117,23 +127,21 @@ class UserHome(webapp2.RequestHandler):
         user = users.get_current_user()
         
         if user:
-            userID = user.user_id()
             template_url= 'user_home_page.html'
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
         else:
-            userID = ""
-            template_url = 'home_page.html'
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
+            # template_url = 'home_page.html'
+            # url = users.create_login_url(self.request.uri)
+            # url_linktext = 'Login'
+            self.redirect('/')
 
-        blog_query= db.GqlQuery("SELECT * FROM Blog " +
-                "WHERE authorID = :1", userID)
-        blogs = blog_query.run(limit=1000)
+        blog_query= getBlogs(userID)
+
+        blogs = blog_query.run(limit=100)
 
         template_values = { 
             'user' : user,
-            'userID' : userID,
             'url': url,
             'url_linktext': url_linktext,
             'blogs' : blogs
@@ -145,14 +153,14 @@ class UserHome(webapp2.RequestHandler):
 class BlogHome(webapp2.RequestHandler):
 
     def post(self, authorID, blog_name, page_number):
-        if self.request.get('blogpost_title') and self.request.get('blogpost_content'):
-            blogpost_title = self.request.get('blogpost_title')
-            blogpost_content = self.request.get('blogpost_content')
-            
+        blogpost_title = self.request.get('blogpost_title')
+        blogpost_content = self.request.get('blogpost_content')
+        if blogpost_title and blogpost_content:
+
             blogpost_tags = self.request.get('blogpost_tags')
             tag_tokens = [tag.strip() for tag in blogpost_tags.split(',')]
 
-            blogpost = Blogpost(authorID=users.get_current_user().user_id(), title=blogpost_title, content=blogpost_content, blog=blog_name, tags=tag_tokens)
+            blogpost = Blogpost(authorID=authorID, title=blogpost_title, content=blogpost_content, blog=blog_name, tags=tag_tokens)
             blogpost.put()
 
             self.redirect('/blog/' + authorID + '/' + blog_name + '/')
@@ -169,28 +177,29 @@ class BlogHome(webapp2.RequestHandler):
 
         user = users.get_current_user()
         if user:
+            userID = user.user_id()
             log_in_out_url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
         else:
+            userID = ""
             template_url = 'home_page.html'
             log_in_out_url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
 
-        one_blog_query = db.GqlQuery("SELECT * FROM Blog " +
-                "WHERE title = :1 AND authorID = :2" , blog_name, authorID)
+        one_blog_query = getBlogs(authorID, blog_name)
 
-        one_blog = one_blog_query.run(limit=1)
-        
+        blogFound = False
+        authorNickname = "error"
         owner = False
-        authorNickname = "Error"
-        for b in one_blog:
-            uno_blog = "hi"
-            owner = (str(user.user_id()) == str(b.authorID))
+
+        for b in one_blog_query.run(limit=1):
+            blogFound = True
+            owner = (user and (user.user_id() == b.authorID))
+            # owner = (str(user.user_id()) == str(b.authorID))
             authorNickname = b.authorNickname
 
-        blogpost_query = db.GqlQuery("SELECT * FROM Blogpost " +
-                "WHERE blog = :1 AND authorID = :2 " +
-                "ORDER BY date DESC", blog_name, authorID)
+        # if blogFound:
+        blogpost_query = getBlogPosts(authorID, blog_name)
 
         blogposts = blogpost_query.run(offset=(ViewingPage+1)*10)
 
@@ -225,15 +234,16 @@ class BlogHome(webapp2.RequestHandler):
                     blog_tags.append(tag)
         blog_tags.sort()
             
+        if user:
+            blog_query = getBlogs(user.user_id())
+            blogs = blog_query.run(limit=1000)    
 
-        blog_query = db.GqlQuery("SELECT * FROM Blog " +
-                "WHERE authorID = :1 " +
-                "ORDER BY title", user.user_id())
-        blogs = blog_query.run(limit=1000)    
+        else:
+            blogs = []
 
         template_values = { 
             'user' : user,
-            'userID': user.user_id(),
+            'userID': userID,
             'url': log_in_out_url,
             'url_linktext': url_linktext,
             'blogs' : blogs,
@@ -241,7 +251,6 @@ class BlogHome(webapp2.RequestHandler):
             'blog_name': blog_name,
             'authorID': authorID,
             'authorNickname': authorNickname,
-            'one_blog': one_blog,
             'blogpost_content' : blogpost_content,
             'blog_tags': blog_tags,
             'owner' : owner,
@@ -251,31 +260,36 @@ class BlogHome(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template("blog_home_page.html")
         self.response.write(template.render(template_values))
 
+        # else:
+        #     self.redirect('/error/')
+
 class BlogpostPage(webapp2.RequestHandler):
+
+    def isLink(self,tok):
+        """Rudimentary url check"""
+        tok = tok.lower()
+        return (tok.startswith("http://") or tok.startswith("https://") 
+            or tok.startswith("www") or tok.endswith(".edu") or tok.endswith(".com")
+            or tok.endswith(".html") or tok.endswith(".jpg") or tok.endswith(".png")
+            or tok.endswith(".gov") or tok.endswith(".org") or tok.endswith(".net")
+            or tok.endswith(".int") or tok.endswith(".mil"))
+
+
     def post(self, authorID, blog_name, blogpost_name, mode):
 
         user = users.get_current_user()
         owner = False
 
-        one_blog_query = db.GqlQuery("SELECT * FROM Blog " +
-                "WHERE title = :1 AND authorID = :2", blog_name, authorID)
+        one_blog_query = getBlogs(authorID, blog_name)
 
         one_blog = one_blog_query.run(limit=1)
         
         for blog in one_blog:
-            if user.user_id() == blog.authorID:
-                owner = True
-            else:
-                owner = False
+            owner = (user and (user.user_id() == blog.authorID))
 
-        if mode == "edit" and owner == True:
-            edit = True
-        else:
-            edit = False
+        edit = (mode == "edit" and owner == True)
 
-        blogpost_query = db.GqlQuery("SELECT * FROM Blogpost " +
-                "WHERE blog = :1 AND title = :2 AND authorID = :3" +
-                "ORDER BY date DESC", blog_name, blogpost_name, authorID)
+        blogpost_query = getBlogPosts(authorID, blog_name, blogpost_name)
 
         blogpost = blogpost_query.run(limit=1)
         new_title = self.request.get('blogpost_title')
@@ -287,63 +301,56 @@ class BlogpostPage(webapp2.RequestHandler):
             for post in blogpost:
                 post.update(new_title, new_content, new_tags)
 
-        self.redirect('/post/'+ blog_name+'/'+new_title+"/view")
+        self.redirect('/post/'+authorID+'/'+blog_name+'/'+new_title+"/view")
 
     def get(self, authorID, blog_name, blogpost_name, mode):
 
         user = users.get_current_user()
         owner = False
 
-        if users.get_current_user():
+        if user:
+            userID = user.user_id()
             log_in_out_url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
         else:
+            userID = ""
             template_url = 'home_page.html'
             log_in_out_url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
 
-        one_blog_query = db.GqlQuery("SELECT * FROM Blog " +
-                "WHERE title = :1", blog_name)
+        one_blog_query = getBlogs(authorID, blog_name)
 
         one_blog = one_blog_query.run(limit=1)
         
         for blog in one_blog:
-            if user.user_id() == blog.authorID:
-                owner = True
-            else:
-                owner = False
+            owner = (user and (user.user_id() == blog.authorID))
+                
+        edit = (mode == "edit" and owner)
 
-        if mode == "edit" and owner == True:
-            edit = True
-        else:
-            edit = False
+        blog_tags_query = getBlogPosts(authorID, blog_name)
 
-        blog_tags_query = db.GqlQuery("SELECT * FROM Blogpost " +
-                "WHERE blog = :1 " +
-                "ORDER BY date DESC", blog_name)
-
-        blogpost_query = db.GqlQuery("SELECT * FROM Blogpost " +
-                "WHERE blog = :1 AND title = :2 " +
-                "ORDER BY date DESC", blog_name, blogpost_name)
+        blogpost_query = getBlogPosts(authorID, blog_name, blogpost_name)
 
         blog_tags=[]
         for post in blog_tags_query.run(limit=1000):
             for tag in post.tags:
                 if tag not in blog_tags:
                     blog_tags.append(tag)
+        blog_tags.sort()
 
         for post in blogpost_query.run(limit=1):
             post.content.split(' ')
             text_tokens = post.content.split(' ')
             for tok in text_tokens:
-                if tok.startswith("http://") or tok.startswith("https://"):
+                if self.isLink(tok):
                     post.content=post.content.replace(tok,to_link(tok))
             blogpost = post
 
-        blog_query = db.GqlQuery("SELECT * FROM Blog " +
-                "WHERE author = :1 " +
-                "ORDER BY title", user)
-        blogs = blog_query.run(limit=1000)   
+        if user:
+            blog_query = getBlogs(user.user_id())
+            blogs = blog_query.run(limit=1000)   
+        else:
+            blogs = []
 
         template_values = { 
             'user' : user,
@@ -374,7 +381,7 @@ class TagSearchPage(webapp2.RequestHandler):
         user = users.get_current_user()
         owner = False
 
-        if users.get_current_user():
+        if user:
             log_in_out_url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
         else:
@@ -382,20 +389,18 @@ class TagSearchPage(webapp2.RequestHandler):
             log_in_out_url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
 
-        one_blog_query = db.GqlQuery("SELECT * FROM Blog " +
-                "WHERE title = :1", blog_name)
+        one_blog_query = getBlogs(authorID, blog_name)
 
         one_blog = one_blog_query.run(limit=1)
         
         for blog in one_blog:
-            if user.user_id() == blog.authorID:
-                owner = True
-            else:
-                owner = False
+            owner = (user and (user.user_id() == blog.authorID))
         
-        blogpost_query = db.GqlQuery("SELECT * FROM Blogpost " +
-                "WHERE blog = :1 AND tags = :2 " +
-                "ORDER BY date DESC", blog_name, tag_name)
+        blogpost_query = getBlogPosts(authorID, blog_name, None, tag_name)
+
+         # db.GqlQuery("SELECT * FROM Blogpost " +
+         #        "WHERE blog = :1 AND tags = :2 AND authorID = :3 " +
+         #        "ORDER BY date DESC", blog_name, tag_name, authorID)
 
         blogposts = blogpost_query.run(offset=(ViewingPage+1)*10)
 
@@ -424,19 +429,20 @@ class TagSearchPage(webapp2.RequestHandler):
                 if tok.startswith("http://") or tok.startswith("https://"):
                     blogpost_content[key]=blogpost_content[key].replace(tok,to_link(tok))
 
-        blog_tags_query = db.GqlQuery("SELECT * FROM Blogpost " +
-                "WHERE blog = :1 " +
-                "ORDER BY date DESC", blog_name)
+        blog_tags_query = getBlogPosts(authorID, blog_name) 
+
         blog_tags=[]
+
         for post in blog_tags_query.run(limit=1000):
             for tag in post.tags:
                 if tag not in blog_tags:
                     blog_tags.append(tag)
+        if user:
+            blog_query = getBlogs(user.user_id())
+            blogs = blog_query.run(limit=1000)   
 
-        blog_query = db.GqlQuery("SELECT * FROM Blog " +
-                "WHERE author = :1 " +
-                "ORDER BY title", user)
-        blogs = blog_query.run(limit=1000)   
+        else:
+            blogs = []
 
         template_values = { 
             'user' : user,
