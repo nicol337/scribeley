0f25da190389ba651bf970d15d2947d0ea1b6944
nicol337 <nkl9092@gmail.com>
2016-01-15 16:25:36 -0500

    added url safe blog names

--- blog.py
+++ blog.py
@@ -1,5 +1,5 @@
 # ??? add redirect to username page if a user signs in on any page and doesn't have a username picked yet.
-
+# lsof -i :<port number> then kill with pid
 import os
 import cgi
 import urllib
@@ -42,6 +42,9 @@ def getBlogPostsQuery(authorname, blog_name, blog_post_name = None, tag_name = N
         return db.GqlQuery("SELECT * FROM Blogpost " +
             "WHERE authorname = :1 AND blog = :2 " +
             "ORDER BY date DESC", authorname, blog_name)
+def getBlogPostsQueryByID(blog_id):
+    return db.GqlQuery("SELECT * FROM Blogpost " +
+        "WHERE ")
 
 def getUsername(userID):
     user_pref_query = db.GqlQuery("SELECT * FROM UserPref " +
@@ -66,6 +69,7 @@ class UserPref(db.Model):
 class Blog(db.Model):
     authorname = db.StringProperty(required=True)
     title = db.StringProperty(required=True)
+    urltitle = db.StringProperty(required=True)
 
 class Blogpost(db.Model):
     authorname = db.StringProperty(required=True)
@@ -189,8 +193,8 @@ class UsernamePage(webapp2.RequestHandler):
 
 class UserHome(webapp2.RequestHandler):
     def isInvalid(self,new_blog_name):
-        invalidCharacters = re.compile('[^a-zA-Z0-9-]+')
-        return bool(invalidCharacters.search(username))
+        invalidCharacters = re.compile('[^a-zA-Z0-9 ]+')
+        return bool(invalidCharacters.search(new_blog_name))
 
     def post(self):
         """Create a new blog for the logged-in user."""
@@ -202,16 +206,20 @@ class UserHome(webapp2.RequestHandler):
             blog_name_query = getBlogsQuery(username, new_blog_name)
             duplicateBlogFound = bool(blog_name_query.count())
 
-            if isInvalid(new_blog_name):
+            if self.isInvalid(new_blog_name):
                 error = new_blog_name+" is an invalid blog name"
+                self.get(error)
+            elif len(new_blog_name) > 30:
+                error = new_blog_name+" is too long"
                 self.get(error)
             elif duplicateBlogFound:
                 error = 'You already have a blog titled '+new_blog_name
                 self.get(error)
             else:
-                new_blog = Blog(authorname=username,title=new_blog_name)
+                new_blog_urltitle = new_blog_name.replace(' ','-')
+                new_blog = Blog(authorname=username,title=new_blog_name,urltitle=new_blog_urltitle)
                 new_blog.put()
-                self.redirect('/blog/'+username+'/'+new_blog_name+'/')
+                self.redirect('/blog/'+username+'/'+new_blog_urltitle+'/')
                 
         else:
             self.redirect('/user/')
@@ -250,7 +258,8 @@ class UserHome(webapp2.RequestHandler):
 
 class BlogHome(webapp2.RequestHandler):
 
-    def post(self, authorname, blog_name, page_number):
+    def post(self, authorname, blog_urltitle, page_number):
+        blog_name = blog_urltitle.replace('-',' ')
         blogpost_title = self.request.get('blogpost_title')
         blogpost_content = self.request.get('blogpost_content')
         user = users.get_current_user()
@@ -264,13 +273,13 @@ class BlogHome(webapp2.RequestHandler):
             blogpost = Blogpost(authorname=username, title=blogpost_title, content=blogpost_content, blog=blog_name, tags=tag_tokens)
             blogpost.put()
 
-            self.redirect('/blog/' + username + '/' + blog_name + '/')
+            self.redirect('/blog/' + username + '/' + blog_urltitle + '/')
         else:
-            self.redirect('/blog/' + username + '/' + blog_name + '/')
+            self.redirect('/blog/' + username + '/' + blog_urltitle + '/')
 
 
-    def get(self, authorname, blog_name, page_number=0):
-
+    def get(self, authorname, blog_urltitle, page_number=0):
+        blog_name = blog_urltitle.replace('-',' ')
         user = users.get_current_user()
 
         if page_number:
@@ -344,6 +353,7 @@ class BlogHome(webapp2.RequestHandler):
             'blogs' : blogs,
             'blogposts' : blogposts,
             'blog_name': blog_name,
+            'blog_urltitle': blog_urltitle,
             'authorname': authorname,
             'blogpost_content' : blogpost_content,
             'blog_tags': blog_tags,
@@ -369,8 +379,8 @@ class BlogpostPage(webapp2.RequestHandler):
             or tok.endswith(".gov") or tok.endswith(".org") or tok.endswith(".net")
             or tok.endswith(".int") or tok.endswith(".mil"))
 
-
-    def post(self, authorname, blog_name, blogpost_name, mode):
+    def post(self, authorname, blog_urltitle, blogpost_name, mode):
+        blog_name = blog_urltitle.replace('-',' ')
 
         user = users.get_current_user()
         username = getUsername(user.user_id())
@@ -393,16 +403,17 @@ class BlogpostPage(webapp2.RequestHandler):
         new_title = self.request.get('blogpost_title')
         new_content = self.request.get('blogpost_content')
         tag_str = self.request.get('blogpost_tags')
-        new_tags = tag_str.split(',')
+        new_tags = [tag for tag in tag_str.split(',') if not tag.strip() == '']
+    
 
         if edit and owner:
             for post in blogpost:
                 post.update(new_title, new_content, new_tags)
 
-        self.redirect('/post/'+authorname+'/'+blog_name+'/'+new_title+"/view")
+        self.redirect('/post/'+authorname+'/'+blog_urltitle+'/'+new_title+"/view")
 
-    def get(self, authorname, blog_name, blogpost_name, mode):
-
+    def get(self, authorname, blog_urltitle, blogpost_name, mode):
+        blog_name = blog_urltitle.replace('-',' ')
         user = users.get_current_user()
 
         owner = False
@@ -443,6 +454,7 @@ class BlogpostPage(webapp2.RequestHandler):
                     blog_tags.append(tag)
         blog_tags.sort()
 
+        #not entering this loop after edit
         for post in blogpost_query.run(limit=1):
             post.content.split(' ')
             text_tokens = post.content.split(' ')
@@ -459,6 +471,7 @@ class BlogpostPage(webapp2.RequestHandler):
             'blogs' : blogs,
             'blogpost' : blogpost,
             'blog_name': blog_name,
+            'blog_urltitle': blog_urltitle,
             'authorname': authorname,
             'one_blog': one_blog,
             'owner' : owner,
@@ -471,8 +484,8 @@ class BlogpostPage(webapp2.RequestHandler):
 
 class TagSearchPage(webapp2.RequestHandler):
 
-    def get(self, authorname, blog_name, tag_name, page_number=0):
-
+    def get(self, authorname, blog_urltitle, tag_name, page_number=0):
+        blog_name = blog_urltitle.replace('-',' ')
         if page_number:
             ViewingPage = int(page_number)
         else:
@@ -534,6 +547,7 @@ class TagSearchPage(webapp2.RequestHandler):
             for tag in post.tags:
                 if tag not in blog_tags:
                     blog_tags.append(tag)
+        blog_tags.sort()
 
         template_values = { 
             'user' : user,
@@ -544,6 +558,7 @@ class TagSearchPage(webapp2.RequestHandler):
             'blogposts' : blogposts,
             'blogpost_content': blogpost_content,
             'blog_name': blog_name,
+            'blog_urltitle': blog_urltitle,
             'authorname': authorname,
             'one_blog': one_blog,
             'tag_name' : tag_name,
@@ -561,8 +576,8 @@ class TagSearchPage(webapp2.RequestHandler):
 
 application = webapp2.WSGIApplication([
     ('/', HomePage),
-    (r'/user/', UserHome),
-    (r'/username/', UsernamePage),
+    (r'/user[/]?', UserHome),
+    (r'/username[/]?', UsernamePage),
     (r'/blog/(.*)/(.*)/(.*)', BlogHome), 
     #(r'/blog/([^/]*)/([^/]*)[/(.*)|/]?', BlogHome), 
     # these urls should be supported and not go to 404 page
@@ -570,6 +585,5 @@ application = webapp2.WSGIApplication([
     # /blog/authorname/blogname/
     # /blog/authorname/blogname/pagenumber
     (r'/post/(.*)/(.*)/(.*)/(.*)', BlogpostPage),
-    (r'/search/(.*)/(.*)/(.*)/(.*)', TagSearchPage),
-    ('/error/', ErrorPage)
+    (r'/search/(.*)/(.*)/(.*)/(.*)', TagSearchPage)
 ], debug=True)
